f1db68c231d121619d9271e04f2addd4f664877a
KseniyaBabaryka-creator <kseniabobariko2005@gmail.com>
2024-09-18 14:02:39 +0200

    Script for form validation added

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+	const formBtn = document.querySelector('.form__btn'),
+			inputs = document.querySelectorAll('.form__input'),
+			errors = ['First Name', 'Last Name', 'Email', 'Password'],
+			emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+	formBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		let arr = [];
+
+		inputs.forEach((input, i) => {
+			if (input.value.trim() === '' || input.value.trim() == null){
+				arr.push(errors[i]);
+				unvalidInput(input);
+			}
+		});
+
+		if (arr.length !== 0){
+			alert(`Please fill next fields: ${arr.join(', ')}`);
+		};
+
+		checkMail(inputs[2]);
+
+	});
+
+	function unvalidInput(input){
+		input.classList.add('error');
+				input.addEventListener('input', (e) => {
+					e.target.classList.remove('error');
+				})
+	}
+
+	function checkMail(input){
+		if (!emailPattern.test(input.value.trim())){
+			alert('Please entry valid Email Address');
+			unvalidInput(input);
+		}
+	};
+})
